2b984d7a488cc49eb3f5c93d8e93ebb16b3b1bf5
chem is try <96931510+Bro-Xun@users.noreply.github.com>
2022-04-10 22:26:43 +0800

    Add files via upload
    Add non-input reaction: encode the current `window.location.href`

--- files/projects/Tools/GHuriToJSDuri/FormatGHSrcURI.js
+++ files/projects/Tools/GHuriToJSDuri/FormatGHSrcURI.js
@@ -1,12 +1,18 @@
 //JavaScript Document
 //change "//github.com/*" into jsdelivr URI
 function forghlink(URI){
-	if(URI.match("//github.com/").length == 1 && URI.match("/blob/").length == 1 ){
-		URI = URI.replace("github.com/","cdn.jsdelivr.net/gh/").replace("/blob/","@");
-		return URI;
+	var sURI = window.location.href;
+	if(URI != undefined){
+		sURI = URI;
 	}
-	if(URI.match("//www.github.com/").length == 1 && URI.match("/blob/").length == 1 ){
-		URI = URI.replace("www.github.com/","cdn.jsdelivr.net/gh/").replace("/blob/","@");
-		return URI;
+	else{}
+
+	if(sURI.match("//github.com/").length == 1 && sURI.match("/blob/").length == 1 ){
+		sURI = sURI.replace("github.com/","cdn.jsdelivr.net/gh/").replace("/blob/","@");
+		return sURI;
+	}
+	if(sURI.match("//www.github.com/").length == 1 && sURI.match("/blob/").length == 1 ){
+		sURI = sURI.replace("www.github.com/","cdn.jsdelivr.net/gh/").replace("/blob/","@");
+		return sURI;
 	}
 }
